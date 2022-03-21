2b26227e6b345e645aef09abf18fa803bdfffb11
Arnoldas Starkauskas <arnoldas.starkauskas@swedbank.lt>
2022-03-21 14:32:23 +0200

    live end

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -10,6 +10,16 @@ export class ContainerComponent implements OnInit {
   header: string = 'Header from variable!';
   entryVariable: Entry = { id: 1, text: 'Mano pirmas irasas' };
 
+  entries: Entry[] = [
+    { id: 1, text: 'Mano pirmas irasas' },
+    { id: 2, text: 'Mano kitas irasas' },
+    { id: 3, text: 'Mano trecias irasas' },
+    { id: 4, text: 'Mano ketvirtas irasas' },
+    { id: 5, text: 'Mano penktas irasas' },
+    { id: 6, text: 'Mano sestas irasas' },
+    { id: 9, text: 'Mano paskutinis irasas' },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, OnChanges, AfterViewInit, OnDestroy, Output, SimpleChanges } from '@angular/core';
 import { Entry } from '../types';
 
 @Component({
@@ -6,17 +6,39 @@ import { Entry } from '../types';
   templateUrl: './entry.component.html',
   styleUrls: ['./entry.component.scss']
 })
-export class EntryComponent implements OnInit {
+export class EntryComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
   @Input() entry: Entry|undefined;
   @Output() onClick: EventEmitter<string> = new EventEmitter<string>();
+  myRandomText: string = '';
 
-  constructor() { }
+  constructor() {
+    console.log('entry constructor', this.entry);
+  }
 
   ngOnInit(): void {
+    console.log('entry ngOnInit', this.entry);
+    this.myRandomText = this.getRandomText();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('entry ngOnChanges', changes);
+  }
+
+  ngAfterViewInit() {
+    console.log('entry ngAfterViewInit');
+  }
+
+  ngOnDestroy() {
+    console.log('entry ngOnDestroy');
   }
 
   handleClick() {
     this.onClick.emit(this.entry?.text)
+  }
+
+  getRandomText() {
+    console.log('getRandomText');
+    return 'em ipsum dolor sit amet, consectetur adipisicing elit. Dicta dolores ducimus error facere id, incidun';
   }
 
 }
